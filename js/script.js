3325d774685210c8640597aac8414229f1f74d47
Leandro gomes <107895852+leandrog20@users.noreply.github.com>
2024-10-27 02:01:15 -0300

    Adicionando Modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-
+let modal = document.querySelector(".modal-container")
 let icon = document.querySelector(".icon")
 let btn = document.querySelector(".btn")
+let btnClose = document.querySelector(".btn-close")
 
 btn.addEventListener('mouseover', over)
 btn.addEventListener('mouseout', out)
+btn.addEventListener('click', openModal)
+btnClose.addEventListener("click", closeModal)
 
 function over(){
     icon.style.animation = "bounce infinite .5s"
@@ -12,3 +15,12 @@ function over(){
 function out(){
     icon.style.animation = "none"
 }
+
+function openModal(){
+    modal.style.display = "flex"
+}
+
+function closeModal(){
+    modal.style.display = "none"
+}
+
