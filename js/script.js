86328898a31f20ec5ea008d6e4ba9cde949f3ff1
Leandro gomes <107895852+leandrog20@users.noreply.github.com>
2024-10-27 12:55:30 -0300

    Adicionando Media Queries

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let modal = document.querySelector(".modal-container")
 let icon = document.querySelector(".icon")
 let btn = document.querySelector(".btn")
 let btnClose = document.querySelector(".btn-close")
+let iframe = document.querySelector(".iframe")
+let movie = document.querySelector(".movie")
+const URL = "https://www.youtube.com/embed/wkn-Gb-Fhac?si=MT557ZvYpNwpPHq6"
 
 btn.addEventListener('mouseover', over)
 btn.addEventListener('mouseout', out)
@@ -9,7 +12,7 @@ btn.addEventListener('click', openModal)
 btnClose.addEventListener("click", closeModal)
 
 function over(){
-    icon.style.animation = "bounce infinite .5s"
+    icon.style.animation = "bounce infinite .4s"
 }
 
 function out(){
@@ -17,10 +20,14 @@ function out(){
 }
 
 function openModal(){
+    movie.pause()
     modal.style.display = "flex"
 }
 
 function closeModal(){
+    movie.play()
     modal.style.display = "none"
+    iframe.setAttribute("src", "-")
+    iframe.setAttribute("src", URL)
 }
 
